26c64b65e3ba65c5d7f7791e87cf061bf02cea9e
Setare <zahrasadat.azarmsa@dci.education>
2023-02-23 09:58:21 +0100

    deployment

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import axios from "axios"
+
+
+//load our environment variables into process.env object
+dotenv.config()
+console.log(process.env)
+const app=express()
+const PORT= process.env.PORT || 4000
+
+
+app.get("/",(req,res)=>{
+    res.send(process.env.SECRET_KEY)
+})
+app.get("/weather",async(req,res)=>{
+    //proxy server
+    const response= await axios.get(` https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${process.env.SECRET_KEY}`)
+    res.send(response.data)
+   
+})
+app.listen(PORT,()=>console.log("server is running on port:",PORT))
